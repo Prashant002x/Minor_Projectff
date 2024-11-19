aa8dc589cc3409e7c37c8271d4003a640101f2ec
Prashant002x <127647624+Prashant002x@users.noreply.github.com>
2024-11-19 18:41:49 +0530

    Update app.py

--- app.py
+++ app.py
@@ -70,6 +70,17 @@ def main():
         st.markdown(f"<h3 class='center'>This might be:</h3>", unsafe_allow_html=True)
         st.markdown(f"<h1 class='center'>{predicted_label}</h1>", unsafe_allow_html=True)
         st.markdown(f"<p class='center'>Confidence: {confidence:.2f}%</p>", unsafe_allow_html=True)
-
+st.markdown(
+    """
+    <style>
+    .css-1jc7ptx, .e1ewe7hr3, .viewerBadge_container__1QSob,
+    .styles_viewerBadge__1yB5_, .viewerBadge_link__1S137,
+    .viewerBadge_text__1JaDK {
+        display: none;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
 if __name__ == '__main__':
     main()
